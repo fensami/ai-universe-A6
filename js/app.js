ccf6fc2405ded4102e80f7e0a0403e413ef8fed4
fensami <113832498+fensami@users.noreply.github.com>
2023-03-03 21:37:22 +0600

    modal items add to dynamic html in javascript file

--- js/app.js
+++ js/app.js
@@ -8,6 +8,24 @@ const universeDataLoad = async () => {
 const displayUniverse = aiUniverse =>{
     // console.log(aiUniverse);
     const displayContainer = document.getElementById('display-container');
+    //display 4 cards 
+    
+
+    // document.getElementById('btn-show-all').addEventListener('click', function(){
+    //   console.log('click');
+    //   const showAll = document.getElementById('show-all');
+    //   if(dataLimit && aiUniverse.length > 4){
+    //     aiUniverse = aiUniverse.slice(0,4);
+    //     showAll.classList.remove('d-none')
+    //   }
+    //   else{
+    //     showAll.classList.add('d-none')
+    //   }
+    // })
+
+
+
+
     aiUniverse.forEach(universe => {
         // console.log(universe);
         const universeDiv = document.createElement('div')
@@ -29,8 +47,9 @@ const displayUniverse = aiUniverse =>{
                         <p>Date:${universe.published_in} </p>
                             </div>
                           <div>
-                          <button onclick="loadUniverseDetails('${universe.id}')" data-bs-toggle="modal" data-bs-target="#universeDetailsModal"> click</button>
+                          <button onclick="loadPhoneDetails('${universe.id}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#universeDetailModal"> Show Ditails </button>
                           </div>
+                     
                           
                         </article>
                   </div>
@@ -40,30 +59,69 @@ const displayUniverse = aiUniverse =>{
     })
 }
 
-const loadUniverseDetails = async id => {
-    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`
-    const res = await fetch(url);
-    const data = await res.json();
-    console.log(data.data);
+const loadPhoneDetails = async id => {
+  // console.log(id);
+  const url =`https://openapi.programming-hero.com/api/ai/tool/${id}`
+  const res = await fetch(url);
+  const data = await res.json();
+  displayPhoneDetails(data.data);
 }
-// const displayUniverseDetails = universe =>{
-//     console.log(universe);
-//     const universeDitalsModal = document.getElementById('universeDetailsModalLabel')
-//     universeDitalsModal.innerText = universe.name;
+const displayPhoneDetails = universeId => {
+  console.log(universeId);
+  
+  // const modalTitle =document.getElementById('phoneDetailModalLabel');
+  // modalTitle.innerText = universeId.description;
+  const universeDetails = document.getElementById('universe-details')
+     universeDetails.innerHTML = `
+     <div class="container text-center">
+     <div class="row row-cols-2">
+ <div class="col bg-danger bg-opacity-10 rounded-3 g-col-6">
+       <h4 class="mt-2">${universeId.description}</h4>
+       <div class="row row-cols-3 p-2 ">
+         <div class="col bg-white rounded-3">${universeId.pricing[0].price}</div>
+         <div class="col bg-white rounded-3 ml-2">${universeId.pricing[1].price}</div>
+         <div class="col bg-white rounded-3 ml-2">${universeId.pricing[2].price}</div>
+       </div>
 
-// }
+       <!-- article -->
+       <article class="row row-cols-2">
+         <div class="col">
+             <h4>Features</h4>
+             <ul>
+                 <li></li>
+                 <li>2</li>
+                 <li>3</li>
+             </ul>
+         </div>
 
-// const displayPhoneDetails = phone => {
-//     console.log(phone);
-//     const modalTitle =document.getElementById('phoneDetailModalLabel');
-//     modalTitle.innerText = phone.name;
-//     const phoneDetails = document.getElementById('phone-details')
-//     phoneDetails.innerHTML = `
-//     <p> Relase date : ${phone.releaseDate ? phone.releaseDate : 'No release Date found'} </P>
-//     <p> Stroge: ${phone.mainFeatures.storage}</p>
-//     <p> Brand: ${phone.brand} </p>
-//     `
-// }
+         <div class="col">
+             <h4>Inegrations</h4>
+             <ul>
+                 <li>${universeId.integrations[0]}</li>
+                 <li>${universeId.integrations[1]}</li>
+                 <li>${universeId.integrations[2]}</li>
+             </ul>
+
+         </div>
+
+       </article>
+
+ </div>
+       
+
+
+ <div class="col rounded-3 border ml-5">
+         <img class="img-fluid mt-2 rounded" src="${universeId.image_link[1]}" alt="">
+         <p>${universeId.input_output_examples[0].input}
+         </p>
+         <p>${universeId.input_output_examples[0].output}
+         </p>
+     </div>
+     </div>
+   </div>
+  `
+  
+}
 
 
 universeDataLoad();
